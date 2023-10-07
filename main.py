05883c17d19b8f4b8ed02bd3cc7d8603b13245cd
Quater <2429386182@qq.com>
2023-10-07 21:31:40 +0800

    10_7_new

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import shutil
-from flask import Flask, request
+from flask import Flask, request, render_template, url_for
 from flask_socketio import SocketIO
 import os
 from threading import Lock
+import subprocess
 
 app = Flask(
     __name__,
-    template_folder='.',  # 模板文件
-    static_folder='.',  # 虚拟资源入口
+    template_folder='templates',  # 模板文件
+    static_folder='',  # 虚拟资源入口
     static_url_path='',
 )
 thread = None
@@ -36,35 +37,53 @@ def clear_folder(folder_path):
 
 @app.route('/upload_2', methods=['POST'])
 def efficient():
+    # 快速
     if 'file' not in request.files:
         return "没有选择文件"
 
     file = request.files['file']
     filename = file.filename
 
-    selected_scale = request.form['scale']  # 获取放大倍数
+    selected_scale = int(request.form['scale'])  # 获取放大倍数
+    print(selected_scale)
+    print(type(selected_scale))
     if not selected_scale:
         print('无放大倍数')
         selected_scale = 2
 
     # 保存文件到指定目录
-    save_path = './video'
+    save_path = 'data'
     file_abs_path = os.path.join(save_path, filename)
     file.save(file_abs_path)
 
     # 以文件绝对地址代替上传文件内容
     file_input = file_abs_path
-    clear_folder("./video/frame")
-    clear_folder("./video/output")
-    os.system(f"ffmpeg -i {file_input} ./video/frame/%05d.jpg -y")
+    clear_folder("data/frame")
+    clear_folder("data/output")
+    os.system(f"ffmpeg -i {file_input} ./data/frame/%05d.jpg -y")
     print("视频抽帧完成！")
     socketio.emit("server_response_2", {'data': "视频抽帧完成"})
-    os.system(
-        f'../realcugan/realcugan-ncnn-vulkan.exe -i ./video/frame -s {selected_scale} -o ./video/output')
+
+    # os.system(f'./realcugan/realcugan-ncnn-vulkan.exe -i ./data/frame -s {selected_scale} -o ./data/output')
+    # print(f'./realcugan/realcugan-ncnn-vulkan.exe -i ./data/frame -s {selected_scale} -o ./data/output')
+    # 调用 .exe 文件并传递参数
+    process = subprocess.run(f'./realcugan/realcugan-ncnn-vulkan.exe -i ./data/frame -s {selected_scale} -o ./data/output',
+                             capture_output=True, text=True)
+    # 输出命令的输出和错误信息
+    print(process.stdout)
+    print(process.stderr)
     socketio.emit("server_response_2", {'data': "视频超分完成"})
-    os.system(f"ffmpeg -i ./video/output/%05d.png -pix_fmt yuv420p -c:v libx264 ./video/output.mp4 -y")
-    socketio.emit('server_response_2', {'data': f"Realcugan超分{selected_scale}倍结果保存至./video/output.mp4"})
-    return f"已对 {filename} 进行超分，并保存到 output.mp4"
+    socketio.emit('server_response_2', {'data': f"Realcugan超分{selected_scale}倍结果保存至./data/output"})
+
+    os.system(f"ffmpeg -i ./data/output/%05d.png -pix_fmt yuv420p -c:v libx264 ./data/output.mp4 -y")
+    socketio.emit("server_response_2", {'data': "视频帧压缩为视频完成"})
+    socketio.emit('server_response_2', {'data': f"Realcugan超分{selected_scale}倍结果保存至./data/output.mp4"})
+
+    os.system(f"ffmpeg -i ./data/frame/%05d.jpg -vf scale=iw*{selected_scale}:ih*{selected_scale} "
+              f"./data/direct/%05d.jpg -y")
+    socketio.emit('server_response', {'data': f"FFmpeg直接放大{selected_scale}倍结果保存至./data/direct"})
+
+    return f"已对 {filename} 进行超分，并保存到 ./data/output.mp4"
 
 
 @app.route("/upload_1", methods=['POST'])
@@ -75,33 +94,41 @@ def quality():
     file = request.files['file']
     filename = file.filename
     # 保存文件到指定目录
-    save_path = './video'
+    save_path = 'data'
     file_abs_path = os.path.join(save_path, filename)
     file.save(file_abs_path)
     file_input = file_abs_path
-    clear_folder("./video/frame")
-    clear_folder("./video/direct")
-    clear_folder("./video/output")
+    clear_folder("data/frame")
+    clear_folder("data/direct")
+    clear_folder("data/output")
     # 视频抽帧
-    os.system(f"ffmpeg -i {file_input} ./video/frame/%05d.jpg -y")
+    os.system(f"ffmpeg -i {file_input} ./data/frame/%05d.jpg -y")
     print("视频抽帧完成！")
     socketio.emit('server_response', {'data': "视频抽帧完成"})
+
     # 输入文件夹进行超分
-    os.system("python ../inference_realbasicvsr.py ../configs/realbasicvsr_x4.py ../checkpoints/RealBasicVSR_x4.pth "
-              "./video/frame ./video/output --max_seq_len 8")
-    socketio.emit('server_response', {'data': "视频超分完成！"})
+    os.system("python ./realbasicvsr/inference_realbasicvsr.py ./realbasicvsr/configs/realbasicvsr_x4.py "
+              "./realbasicvsr/checkpoints/RealBasicVSR_x4.pth ./data/frame ./data/output --max_seq_len 8")
+    socketio.emit('server_response', {'data': "视频超分完成"})
+    socketio.emit('server_response', {'data': "RealBasicVSR_x4超分结果保存至./data/output"})
+
+    os.system(f"ffmpeg -i ./data/output/%05d.png -pix_fmt yuv420p -c:v libx264 ./data/output.mp4 -y")
+    socketio.emit("server_response_2", {'data': "视频帧压缩为视频完成"})
+    socketio.emit('server_response_2', {'data': f"RealBasicVSR_x4超分倍结果保存至./data/output.mp4"})
+
     #  直接放大，方便对比效果
-    os.system("ffmpeg -i ./video/frame/%05d.jpg -vf scale=iw*4:ih*4 ./video/direct/%05d.jpg -y")
-    socketio.emit('server_response', {'data': "视频抽帧结果保存至./video/frame"})
-    socketio.emit('server_response', {'data': "RealBasicVSR_x4超分结果保存至./video/output"})
-    socketio.emit('server_response', {'data': "FFmpeg直接放大结果保存至./video/direct"})
-    return f"已对 {filename} 进行超分，并保存到 ./video/output"
+    os.system("ffmpeg -i ./data/frame/%05d.jpg -vf scale=iw*4:ih*4 ./data/direct/%05d.jpg -y")
+    socketio.emit('server_response', {'data': "视频抽帧结果保存至./data/frame"})
+    socketio.emit('server_response', {'data': "FFmpeg直接放大结果保存至./data/direct"})
+
+    return f"已对 {filename} 进行超分，并保存到 ./data/output"
 
 
 @app.route("/")
 def index():
-    with open('index.html', encoding='utf8') as f:
-        return "".join(f.readlines())
+    # with open('./templates/index.html', encoding='utf-8') as f:
+    #     return "".join(f.readlines())
+    return render_template('index.html')
 
 
 # 后端程序
